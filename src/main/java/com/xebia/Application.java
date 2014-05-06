2df230bebda41172e5b1f82aaf25553adaeac3ff
njozwiak <njozwiak@xebia.fr>
2014-05-06 13:14:50 +0200

    Export configuration in file

--- src/main/java/com/xebia/Application.java
+++ src/main/java/com/xebia/Application.java
@@ -1,11 +1,9 @@
 package com.xebia;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
-@ComponentScan
-@EnableAutoConfiguration
+@Import(DefaultConfig.class)
 public class Application {
 
     public static void main(String[] args) {
