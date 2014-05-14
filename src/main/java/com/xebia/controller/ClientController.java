c70b52df8971e893b8afb9e5b2038a1e9965a70a
njozwiak <njozwiak@xebia.fr>
2014-05-14 12:00:43 +0200

    Add view to clients

--- src/main/java/com/xebia/controller/ClientController.java
+++ src/main/java/com/xebia/controller/ClientController.java
@@ -2,13 +2,21 @@ package com.xebia.controller;
 
 import com.xebia.domain.Client;
 import com.xebia.service.ClientService;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
@@ -36,6 +44,12 @@ public class ClientController {
         }else{
             return new ResponseEntity<>(clients,OK);
         }
+    }
+
+    @RequestMapping(value="/client.html", method = RequestMethod.GET)
+    public Model getListClients(Model model) {
+      model.addAttribute("clients", clientService.findAll());
+      return model;
     }
 
     @RequestMapping(value = "/client", method = GET)
